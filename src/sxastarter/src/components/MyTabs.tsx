6210f170b3fedd7159f1d3068e3c01f80bde266e
yavorsk <yavor.krastev@sitecore.com>
2024-05-09 13:38:36 +0300

    update my tabs

--- src/sxastarter/src/components/MyTabs.tsx
+++ src/sxastarter/src/components/MyTabs.tsx
@@ -1,12 +1,15 @@
 import { Field, withDatasourceCheck, Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import {
+  resetEditorChromes
+} from '@sitecore-jss/sitecore-jss-nextjs/utils';
 
 type TabsProps = ComponentProps & {
   fields: {
     heading: Field<string>;
   };
-  tabs: [0, 1];
+  tabs: [0, 1]
 };
 
 const MyTabs = (props: TabsProps): JSX.Element => {
@@ -14,27 +17,40 @@ const MyTabs = (props: TabsProps): JSX.Element => {
 
   const handleTabClick = (index: number) => {
     setActiveTab(index);
+    resetEditorChromes();
+    console.log('hi from handle click tab: ' + index);
   };
+
+  useEffect(() => {
+    resetEditorChromes();
+    console.log('hi from use effect')
+  }, [activeTab]);
+  
   return (
-    <div>
-      <div className="tab-buttons">
-        {[0, 1].map((tab, index) => (
-          <button
-            key={index}
-            data-tab={tab}
-            className={index === activeTab ? 'active' : ''}
-            onClick={() => handleTabClick(index)}
-          >
-            {index}
-          </button>
-        ))}
-      </div>
-      <div className="tab-content">
-        {activeTab == 0 && <Placeholder name="tab-container-1" rendering={props?.rendering} />}
-        {activeTab == 1 && <Placeholder name="tab-container-2" rendering={props?.rendering} />}
-      </div>
-    </div>
-  );
+  <div>
+  <div className="tab-buttons">
+    {[0, 1].map((tab, index) => (
+      <button
+        key={index}
+        data-tab={tab}
+        className={index === activeTab ? 'active' : ''}
+        onClick={() => handleTabClick(index)}
+      >
+        {index}
+      </button>
+    ))}
+  </div>
+  <div className="tab-content">
+
+  {activeTab ==0 && 
+      <Placeholder name='tab-container-1' rendering={props?.rendering}/>
+  }
+  {activeTab ==1 && 
+      <Placeholder name='tab-container-2' rendering={props?.rendering}/>
+  }
+  </div>
+</div>
+);
 };
 
 export default withDatasourceCheck()<TabsProps>(MyTabs);
